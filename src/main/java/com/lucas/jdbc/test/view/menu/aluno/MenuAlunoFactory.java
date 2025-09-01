1a155782efc5a53f86c48610fc8451549691c852
Luca5Eckert <ecklukatv@gmail.com>
2025-09-01 18:34:49 -0300

    feat: adiciona implementao do menu factory para usuarios

--- src/main/java/com/lucas/jdbc/test/view/menu/aluno/MenuAlunoFactory.java
+++ src/main/java/com/lucas/jdbc/test/view/menu/aluno/MenuAlunoFactory.java
@@ -1,4 +1,30 @@
 package com.lucas.jdbc.test.view.menu.aluno;
 
-public class MenuAlunoFactory {
+import com.lucas.jdbc.test.view.menu.*;
+
+public class MenuAlunoFactory implements MenuFactory {
+    @Override
+    public MenuBuscar instanceBuscar() {
+        return null;
+    }
+
+    @Override
+    public MenuAdicionar instanceAdicionar() {
+        return null;
+    }
+
+    @Override
+    public MenuEditar instanceEditar() {
+        return null;
+    }
+
+    @Override
+    public MenuRemover instanceRemover() {
+        return null;
+    }
+
+    @Override
+    public MenuListar instanceListar() {
+        return null;
+    }
 }
--- src/main/java/com/lucas/jdbc/test/view/menu/usuario/MenuUsuarioFactory.java
+++ src/main/java/com/lucas/jdbc/test/view/menu/usuario/MenuUsuarioFactory.java
@@ -1,4 +1,40 @@
 package com.lucas.jdbc.test.view.menu.usuario;
 
-public class MenuUsuarioFactory {
+import com.lucas.jdbc.test.infra.utils.BeanUtil;
+import com.lucas.jdbc.test.view.menu.*;
+
+public class MenuUsuarioFactory implements MenuFactory {
+    private final static MenuAdicionar MENU_ADICIONAR = new MenuAdicionarUsuario(BeanUtil.instanceUsuarioService());
+    private final static MenuRemover MENU_REMOVER = new MenuRemoverUsuario(BeanUtil.instanceUsuarioService());
+    private final static MenuEditar MENU_EDITAR = new MenuEditarEmailUsuario(BeanUtil.instanceUsuarioService());
+    private final static MenuListar MENU_LISTAR = new MenuListarUsuarios(BeanUtil.instanceUsuarioService());
+    private final static MenuBuscar MENU_BUSCAR = new MenuBuscarPorIdUsuario(BeanUtil.instanceUsuarioService());
+
+
+
+    @Override
+    public MenuBuscar instanceBuscar() {
+        return MENU_BUSCAR;
+    }
+
+    @Override
+    public MenuAdicionar instanceAdicionar() {
+        return MENU_ADICIONAR;
+    }
+
+    @Override
+    public MenuEditar instanceEditar() {
+        return MENU_EDITAR;
+    }
+
+    @Override
+    public MenuRemover instanceRemover() {
+        return MENU_REMOVER;
+    }
+
+    @Override
+    public MenuListar instanceListar() {
+        return MENU_LISTAR;
+    }
+
 }
